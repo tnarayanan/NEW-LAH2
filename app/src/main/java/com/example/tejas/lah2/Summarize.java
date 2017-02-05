cba4d0a7e3d0b5fc3627988bb8eb89909111a0a9
Ashboy64 <ashish.arartc@gmail.com>
2017-02-04 20:18:51 -0800

    Summarize

---
+++ app/src/main/java/com/example/tejas/lah2/Summarize.java
@@ -0,0 +1,16 @@
+package com.example.tejas.lah2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Summarize extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summarize);
+    }
+
+
+
+}
