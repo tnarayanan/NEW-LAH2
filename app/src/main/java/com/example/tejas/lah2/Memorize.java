d28971451b40063ac9dac5b107f51f9458590e3f
RazrSlyr <sharpedo75@gmail.com>
2017-02-04 16:22:41 -0800

    made changes to main activity and added memorization activity

--- app/src/main/java/com/example/tejas/lah2/Memorize.java
+++ app/src/main/java/com/example/tejas/lah2/Memorize.java
@@ -1,17 +1,74 @@
 package com.example.tejas.lah2;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import static android.widget.Toast.*;
 
 public class Memorize extends AppCompatActivity {
     TextView textSegment;
+    int counter = 0;
+    Button record;
+    private final int SPEECH_RECOGNITION_CODE = 1;
+    private String txtOutput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        counter = 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memorize);
         textSegment = (TextView)findViewById(R.id.textSegment);
         textSegment.setText(MainActivity.stringSentences.get(0));
+        record = (Button)findViewById(R.id.next);
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSpeechToText();
+            }
+        });
 
+
+    }
+    private void startSpeechToText() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Speak something...");
+        try {
+            startActivityForResult(intent, SPEECH_RECOGNITION_CODE);
+        } catch (ActivityNotFoundException a) {
+            makeText(getApplicationContext(),
+                    "Sorry! Speech recognition is not supported in this device.",
+                    LENGTH_SHORT).show();
+        }
+    }
+    /**
+     * Callback for speech recognition activity
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case SPEECH_RECOGNITION_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String text = result.get(0);
+                    txtOutput = text;
+                }
+                break;
+            }
+        }
     }
 }
